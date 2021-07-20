e92e82f890fdcf11c9e20434175fa479835b745c
Dibyendu Maji <32224234+dibyendu415@users.noreply.github.com>
2021-07-20 21:41:37 +0530

    Update User.java

--- src/com/dibyendu/User.java
+++ src/com/dibyendu/User.java
@@ -6,11 +6,14 @@ import java.util.Scanner;
 
 public class User {
 
-    public static void main(String[] args) {
-	// write your code here
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+
+    // For File Input
         String input;
-        do{
+        FileInputStream file = new FileInputStream("input.txt");
+        Scanner sc = new Scanner(file);
+        while(sc.hasNextLine())
+        {
             input = sc.nextLine();
             String[] commands = input.split(" ");
             Command c;
@@ -21,6 +24,22 @@ public class User {
             {
                 System.out.println("Command not valid");
             }
-        }while(!input.equals("exit"));
+        }
+
+//        For System Input
+//        Scanner sc = new Scanner(System.in);
+//        String input;
+//        do{
+//            input = sc.nextLine();
+//            String[] commands = input.split(" ");
+//            Command c;
+//            try {
+//                c = Command.valueOf(commands[0]);
+//                c.executeCommand(commands);
+//            }catch ( Exception e)
+//            {
+//                System.out.println("Command not valid");
+//            }
+//        }while(!input.equals("exit"));
     }
 }
